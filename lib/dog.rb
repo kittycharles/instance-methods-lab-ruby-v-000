f1dd6a27deff45bf92ef08cc7d2cc6fba2309727
Richard Sanchez <richard.charles.s@gmail.com>
2018-06-06 13:40:13 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,10 @@
-class Dog
-  def bark
-  puts "Woof!"
-  end
-  def sit
-  puts "The Dog is sitting"
-  end
-end
-
-Dog.sit
+# class Dog
+#   def bark
+#   puts "Woof!"
+#   end
+#   def sit
+#   puts "The Dog is sitting"
+#   end
+# end
+#
+# Dog.sit
